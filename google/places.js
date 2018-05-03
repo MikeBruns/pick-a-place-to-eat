df365396859ec5a6c64c8725d5b24edaeb242494
Michael Bruns <michaelbruns99@gmail.com>
2018-05-03 19:36:57 -0400

    breaking out parameter

--- google/places.js
+++ google/places.js
@@ -3,10 +3,11 @@ const config = require('../config.json');
 
 var lat = 41.48754;
 var lng = -82.10153689999999;
+var radiusInMeters = 5000;
 
 var placesNearHere = () => {
   request({
-    url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=5000&type=restaurant&key=${config.googleApiKey}`,
+    url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=${radiusInMeters}&type=restaurant&key=${config.googleApiKey}`,
     json: true
   }, (error, response, body) => {
     console.log(JSON.stringify(body, undefined, 2));

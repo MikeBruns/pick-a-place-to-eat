af6a7506609086bf308aa320e2727587a613336d
Michael Bruns <michaelbruns99@gmail.com>
2018-06-28 15:49:33 -0400

    only getting one page of results logic

--- google/places.js
+++ google/places.js
@@ -5,20 +5,25 @@ var lat = 41.48754;
 var lng = -82.10153689999999;
 // var lat = 41.8781; //chicago
 // var lng = -87.62989;
-var radiusInMeters = 50000;
 // var radiusInMeters = 24140.16;
+var radiusInMeters = 50000;
 const googleMapsLocationUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=';
 var placesNames = [];
+var count = 0;
 
 var placesNearHere = (settings) => {
   request({
     url: `${googleMapsLocationUrl}${lat},${lng}&radius=${settings.radiusInMeters}&opennow&type=restaurant&key=${config.googleApiKey}`,
     json: true
   }, (error, response, body) => {
+    console.log(settings);
     // console.log(JSON.stringify(body, undefined, 2));
     getPlacesNearMeNames(body);
     if (body.next_page_token) {
+      console.log('is true');
       getNextPage(body.next_page_token);
+    } else {
+      determineTheChosenOne();
     }
   });
 };
@@ -29,10 +34,13 @@ var getPlacesNearMeNames = body => {
   for (var i = 0; body.results && i < body.results.length; i++) {
     placesNames.push(body.results[i].name);
   }
-  // console.log(placesNames);
+  console.log(count);
+  console.log(placesNames);
+  count++;
 };
 
 var getNextPage = nextPageToken => {
+  console.log('nextPageToken', nextPageToken);
   setTimeout(() => {
     request({
       url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${config.googleApiKey}&pagetoken=${nextPageToken}`,
@@ -45,15 +53,23 @@ var getNextPage = nextPageToken => {
       if (body.next_page_token) {
         getNextPage(body.next_page_token)
       } else {
-        // console.log('Final places list:');
-        // console.log(placesNames);
-        // console.log(placesNames.length);
-        var theChosenOne = placesNames[Math.floor(Math.random() * placesNames.length)];
-        console.log(`Looks like we are eating at... ${theChosenOne}`);
+        determineTheChosenOne();
       }
     });
   }, 2000); //have to use a timeout to allow the next page work, it only works after it has been processed
 };
+
+const determineTheChosenOne = () => {
+  // console.log('Final places list:');
+  // console.log(placesNames);
+  // console.log(placesNames.length);
+  if (placesNames.length > 0) {
+    let theChosenOne = placesNames[Math.floor(Math.random() * placesNames.length)];
+    console.log(`Looks like we are eating at... ${theChosenOne}`);
+  } else {
+    console.log('Hmm there were no results. Go make a sandwhich.');
+  } 
+}
 
 
 // var googleMapsClient = require('@google/maps').createClient({

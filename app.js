9c835ac790dd6a2a53eb5173da1bd729ccbe9926
Michael Bruns <michaelbruns99@gmail.com>
2018-06-29 14:30:52 -0400

    added address to available parameters

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const yargs = require('yargs');
 const google = require('./google/places.js');
 
-// const argv = yargs
-//   .options({
-//     address: {
-//       demand: true,
-//       alias: 'a',
-//       describe: 'Address to fetch weather for',
-//       string: true
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: 'a',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
 
 console.log('Lets pick a place to eat...');
 
 google.placesNearHere({
   address: '44054',
-  radiusInMeters: 5000
+  radiusInMeters: 50000
 });

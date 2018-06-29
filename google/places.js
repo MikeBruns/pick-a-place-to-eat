cf1296b9bb3dd5651e5e2e978472d54351c15b3f
Michael Bruns <michaelbruns99@gmail.com>
2018-06-29 14:15:15 -0400

    going back to the old timeout. needs to be a promise though.. with a then

--- google/places.js
+++ google/places.js
@@ -19,7 +19,6 @@ var placesNearHere = (settings) => {
     // console.log(JSON.stringify(body, undefined, 2));
     getPlacesNearMeNames(body);
     if (body.next_page_token) {
-      console.log('is true');
       getNextPage(body.next_page_token);
     } else {
       determineTheChosenOne();
@@ -29,20 +28,18 @@ var placesNearHere = (settings) => {
 
 var getPlacesNearMeNames = body => {
   console.log('We are searching for your feast...');
-  // console.log(JSON.stringify(body, undefined, 2));
   for (var i = 0; body.results && i < body.results.length; i++) {
     placesNames.push(body.results[i].name);
   }
 };
 
 var getNextPage = nextPageToken => {
-  console.log('nextPageToken', nextPageToken);
   setTimeout(() => {
     request({
       url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${config.googleApiKey}&pagetoken=${nextPageToken}`,
       json: true
     }, (error, response, body) => {
-      console.log('...');
+      console.log('Fetching more places to eat...');
       for (var i = 0; i < body.results.length; i++) {
         placesNames.push(body.results[i].name);
       }
@@ -52,7 +49,7 @@ var getNextPage = nextPageToken => {
         determineTheChosenOne();
       }
     });
-  }, 1200); //have to use a timeout to allow the next page work, it only works after it has been processed
+  }, 2000); //have to use a timeout to allow the next page work, it only works after it has been processed
 };
 
 const determineTheChosenOne = () => {
@@ -61,7 +58,7 @@ const determineTheChosenOne = () => {
   // console.log(placesNames.length);
   if (placesNames.length > 0) {
     let theChosenOne = placesNames[Math.floor(Math.random() * placesNames.length)];
-    console.log(`Looks like we are eating at... ${theChosenOne}`);
+    console.log(`\nLooks like we are eating at... ${theChosenOne}`);
   } else {
     console.log('Hmm there were no results. Go make a sandwhich.');
   } 
